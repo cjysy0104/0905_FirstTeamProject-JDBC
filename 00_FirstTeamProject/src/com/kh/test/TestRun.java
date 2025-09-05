626e4d1fa5988426c753e87967c892eaef5d2ebb
cjysy0104 <cjysy0104@gmail.com>
2025-09-05 11:42:33 +0900

    SQL Setting

--- 00_FirstTeamProject/src/com/kh/test/TestRun.java
+++ 00_FirstTeamProject/src/com/kh/test/TestRun.java
@@ -3,14 +3,7 @@ package com.kh.test;
 public class TestRun {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-
-
-=======
 		System.out.println("안녕하세요");
-		 
-		
->>>>>>> 4ee6a2f48627ccf8a5df8b4c6e9027e073ac7ff3
 	}
 
 }
