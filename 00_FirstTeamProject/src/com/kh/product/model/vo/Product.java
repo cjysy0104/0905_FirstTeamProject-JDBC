41310fd858f2554cb3a3b04e6392d06b9d44b0f7
cjysy0104 <cjysy0104@gmail.com>
2025-09-05 12:30:49 +0900

    mapper.xml setting

--- 00_FirstTeamProject/src/com/kh/product/model/vo/Product.java
+++ 00_FirstTeamProject/src/com/kh/product/model/vo/Product.java
@@ -1,5 +1,56 @@
 package com.kh.product.model.vo;
 
-public class Product {
+import java.util.Objects;
 
+public class Product {
+	
+	private String prdId;
+	private String prdName;
+	private String category;
+	private int price;
+	private int stock;
+	public Product() {
+		super();
+	}
+	public Product(String prdId, String prdName, String category, int price, int stock) {
+		super();
+		this.prdId = prdId;
+		this.prdName = prdName;
+		this.category = category;
+		this.price = price;
+		this.stock = stock;
+	}
+	public String getPrdId() {
+		return prdId;
+	}
+	public String getPrdName() {
+		return prdName;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(category, prdId, prdName, price, stock);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(category, other.category) && Objects.equals(prdId, other.prdId)
+				&& Objects.equals(prdName, other.prdName) && price == other.price && stock == other.stock;
+	}
+	
+	
 }
--- 00_FirstTeamProject/src/com/kh/product/view/ProductView.java
+++ 00_FirstTeamProject/src/com/kh/product/view/ProductView.java
@@ -1,5 +1,35 @@
 package com.kh.product.view;
 
+import java.util.Scanner;
+
+import com.kh.product.controller.ProductController;
+
 public class ProductView {
 
+	Scanner sc = new Scanner(System.in);
+	ProductController pc = new ProductController();
+	
+	public void prdMenu() {
+		
+		while(true) {
+			
+			System.out.println("상품 목록 메뉴입니다.");
+			System.out.println("1. 전체 상품 목록 조회");
+			System.out.println("2. 특정 상품 목록 조회");
+			System.out.println("3. 신규 상품 목록 기입");
+			System.out.println("4. 특정 상품 삭제");
+			System.out.println("9. 프로그램 종료");
+			try {
+				int menuNo = sc.nextInt();
+				sc.nextLine();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		
+		
+	}
 }
